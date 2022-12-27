7d56da4b593aed9696866cd7df649624aa3868af
Aaron White <w531t4@gmail.com>
2022-12-27 14:25:53 -0500

    add packaging things

---
+++ alarmcenter/__init__.py
@@ -0,0 +1,9 @@
+"""
+alarmcenter
+
+A "little thought invested" approach to ingesting Dahua alarms
+"""
+
+__version__ = "0.0.0"
+__author__ = 'Aaron White'
+__credits__ = ''
---
+++ alarmcenter/command_line.py
@@ -0,0 +1,4 @@
+import alarmcenter
+
+def main():
+    alarmcenter.main()
